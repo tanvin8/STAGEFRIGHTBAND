cc8ff8eb7f52cedb8926cb9a7e26c9bbb04d4fab
tanvin8 <tanvi08.nagilla@gmail.com>
2024-12-18 13:38:13 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend:{
+      colors:{
+        navRed1: '#4b011d',
+        navRed2: '#4b0101',
+        navRed: '#3d0909',
+        pageBlack: '#130007',
+        navBlue: '#001B3A',
+        neonBlue: '#7beaf0',
+        neonPink: '#f710bc',
+        neonPurple: '#452b7e',
+        neonIndigo: '#3d31d5',
+        pageTeal: '#069494'
+      },
+      animation:{
+        scroll:'scroll 15s linear infinite',
+      },
+      keyframes:{
+        scroll:{
+          '0%': {transform: 'translateX(0)'},
+          '100%': {transform: 'translateX(-106%)'},
+        },
+      },
+    },
+  },
+  plugins: [],
+};
